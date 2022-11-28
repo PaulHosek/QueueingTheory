7433c004e73725f79f9f55a7e95872b212235c54
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2022-11-28 11:00:02 +0100

    - logging first draft

--- des_queue.py
+++ des_queue.py
@@ -1,8 +1,11 @@
+import csv
+
 import numpy as np
-import simpy
 import simpy as sip
 from collections import deque
-
+import os
+from datetime import datetime
+from pathlib import Path
 current_queue = deque()
 
 
@@ -11,14 +14,43 @@ class Message():
     Class Message to send to the server. If needs queueing will be queued.
     """
 
-    def __init__(self, env, duration_work, id):
+    def __init__(self, env, duration_work, id, experiment_name):
         self.env = env
         self.duration_work = duration_work
         self.id = id
+        self.experiment_name = experiment_name
 
 
     def __repr__(self):
         return f"This is an instance of class Message with duration work of {self.duration_work}, id {self.id}."
+
+    # private method to avoid name collisions
+    @staticmethod
+    def __log_row(experiment_name,item_id,queue_len, start_request_time,entry_time,exit_time):
+        """Log the current data to the file with the experiment.
+         If the file does not exist, make file and add header row.
+         :return void
+         """
+
+        if not os.path.exists('logged_data'):
+            os.makedirs("logged_data")
+
+        # if file does not exist yet make one and write the column headers in it
+        my_file = Path(os.path.join("logged_data", experiment_name+".csv"))
+
+        if not my_file.is_file():
+            with open(os.path.join("logged_data", experiment_name+".csv"),"w+") as f:
+                wr = csv.writer(f)
+                wr.writerow(["experiment_name","ID","Queue_length","start_request_time","entry_time","exit_time","current_time"])
+
+        # log the data
+        cur_time = datetime.now()
+        with open(os.path.join("logged_data", experiment_name+".csv"), "a") as f:
+
+            wr = csv.writer(f)
+            wr.writerow([experiment_name,item_id,queue_len, start_request_time,entry_time,exit_time,cur_time])
+        return None
+
 
     def request_server(self, env, server):
         """
@@ -26,7 +58,7 @@ class Message():
         Also, log timestamps for waiting time before reaching server/ in queue and total execution time.
         :param env: simpy.environment object
         :param server: server instance to process the message
-        :return:
+        :return void
         """
         start_request_time = env.now
         current_queue.appendleft(self.id)
@@ -36,6 +68,7 @@ class Message():
             yield cur_request
             # Found server
             # remove element if process granted access to server
+            item_id = self.id
             current_queue.remove(self.id)
             entry_time = env.now
             # do job in current message
@@ -44,14 +77,14 @@ class Message():
 
             #####################
             # logging
+            self.__log_row(self.experiment_name,item_id,len(current_queue), start_request_time,entry_time,exit_time)
+            # print("start_request_time", "entry_time", "exit_time", "exit_time - entry_time", "len(current_queue)")
+            # print(start_request_time, entry_time, exit_time, exit_time - entry_time, len(current_queue))
 
-            print("start_request_time", "entry_time", "exit_time", "exit_time - entry_time", "len(current_queue)")
-            print(start_request_time, entry_time, exit_time, exit_time - entry_time, len(current_queue))
-
-            # save results somewhere entry time, exit time, delta_time, len(current_queue)
             # Neet to be done here:
             # save results
             ######################
+            return None
 
 
 class Server(object):
@@ -97,7 +130,7 @@ class Server(object):
         yield self.env.timeout(message.duration_work)
 
 
-def des_simulation(env, n, mu, lamd, mean_duration_work, a, b, queue_type):
+def des_simulation(env, n, mu, lamd, mean_duration_work, a, b, queue_type, experiment_name):
     """
     :param n: number of servers
     :param env: simpy environment
@@ -106,6 +139,7 @@ def des_simulation(env, n, mu, lamd, mean_duration_work, a, b, queue_type):
     :param mean_duration_work:
     :param a: inter-arrival time distribution function with mean(lamb)
     :param b: service-time distribution function with mean (mean_duration_work) generating (random) numbers according to some distibution
+    :param experiment_name: string; name of the logging file
     :return: void
     """
     # Generate as many servers as needed
@@ -116,7 +150,7 @@ def des_simulation(env, n, mu, lamd, mean_duration_work, a, b, queue_type):
     # now the queue can grow up if rho > 1
     while True:
         # generate new message with some duration_work according to b
-        cur_message = Message(env, b(mean_duration_work), id)
+        cur_message = Message(env, b(mean_duration_work), id, experiment_name)
         id += 1
 
         # enter queue and wait until served
@@ -130,9 +164,9 @@ def des_simulation(env, n, mu, lamd, mean_duration_work, a, b, queue_type):
 
 
 def main_des(max_iter, n, mu, lamd, mean_duration_work, a=np.random.exponential, b=np.random.exponential,
-             queue_type=sip.Resource):
+             queue_type=sip.Resource, experiment_name="testing"):
     env = sip.Environment()
-    env.process(des_simulation(env, n, mu, lamd, mean_duration_work, a, b, queue_type))
+    env.process(des_simulation(env, n, mu, lamd, mean_duration_work, a, b, queue_type,experiment_name))
     env.run(until=max_iter)
     print("DES finished.")
 
@@ -146,5 +180,6 @@ if __name__ == "__main__":
     a = np.random.exponential
     b = np.random.exponential
     queue_type = sip.Resource
+    experiment_name = "testing"
 
-    main_des(max_iter, n, mu, lamd, mean_duration_work, a, b, queue_type)
+    main_des(max_iter, n, mu, lamd, mean_duration_work, a, b, queue_type,experiment_name)
