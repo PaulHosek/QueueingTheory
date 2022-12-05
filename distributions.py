3c5133245e694a76316c7a369f87417a3c62e4f2
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2022-12-05 10:10:03 +0100

    - fix hyperexponential

--- distributions.py
+++ distributions.py
@@ -10,7 +10,7 @@ def hyperexponential(seed_1=None, seed_2=None, loopidx=None):
     """
     if loopidx != None:
         seed_1 += loopidx
-        seed_2 += loopidx*2
+        seed_2 += loopidx+seed_1
     rng_1 = np.random.default_rng(seed_1)
     rng_2 = np.random.default_rng(seed_2)
     if rng_2.integers(0,4,1) == 0:
